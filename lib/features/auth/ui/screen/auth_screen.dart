911b845fae9307ef500bb48fb5261d782e6a5548
Taylor Biehn <taylorbiehn@outlook.com>
2025-09-21 09:29:07 +0300

    add blueprint to screen

--- lib/features/auth/ui/screen/auth_screen.dart
+++ lib/features/auth/ui/screen/auth_screen.dart
@@ -4,6 +4,7 @@ import 'package:evira_e_commerce/core/lang_generated/l10n.dart';
 import 'package:evira_e_commerce/core/routes/app_router.dart';
 import 'package:evira_e_commerce/core/theme/app_theme.dart';
 import 'package:evira_e_commerce/shared/cubits/theme_cubit.dart';
+import 'package:evira_e_commerce/shared/widgets/base_stateful_screen.dart';
 import 'package:evira_e_commerce/shared/widgets/custom_button.dart';
 import 'package:evira_e_commerce/shared/widgets/custom_sign_button.dart';
 import 'package:flutter/gestures.dart';
@@ -15,27 +16,32 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:my_flutter_toolkit/core/extensions/context_extensions.dart';
 import 'package:my_flutter_toolkit/ui/widgets/custom_divider.dart';
 
-class AuthScreen extends StatefulWidget {
+class AuthScreen extends BaseStatefulScreen {
   const AuthScreen({super.key});
 
   @override
-  State<AuthScreen> createState() => _AuthScreenState();
+  BaseStatefulScreenState<AuthScreen> createState() => _AuthScreenState();
 }
 
-class _AuthScreenState extends State<AuthScreen> {
+class _AuthScreenState extends BaseStatefulScreenState<AuthScreen> {
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     body: SafeArea(
+  //       child: Padding(
+  //         padding: EdgeInsets.only(left: 20.w, right: 20.w, top: 20.h),
+  //         child: Column(
+  //           crossAxisAlignment: CrossAxisAlignment.start,
+  //           children: const [HeaderSection(), BodySection()],
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.only(left: 20.w, right: 20.w, top: 20.h),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [HeaderSection(), BodySection()],
-          ),
-        ),
-      ),
-    );
+  Widget buildBody(BuildContext context) {
+    return const BodySection();
   }
 }
 
